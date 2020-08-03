adf0a33099039f6d49a102e5163b20ab84b32c3d
vicky <jiajun_work@126.com>
2020-08-03 18:19:17 +0800

    [react-test][vicky] refator: format.

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -4,7 +4,8 @@ class Counter extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: 0
+            value: 0,
+            groupSize: 0
         };
     }
 
@@ -12,12 +13,25 @@ class Counter extends React.Component {
         this.setState((preState) => ({
             value: preState.value + 1
         }))
+        this.props.onIncrease();
     };
 
     onDecrease = () => {
         this.setState((preState) => ({
             value: preState.value - 1
         }))
+        this.props.onDecrease();
+    };
+
+    static getDerivedStateFromProps = (props, state) => {
+        if (props.groupSize !== state.groupSize) {
+            return {
+                value: 0,
+                groupSize: props.groupSize
+            }
+
+        }
+        return null;
     };
 
     render() {
