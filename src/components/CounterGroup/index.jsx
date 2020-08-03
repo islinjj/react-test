90db0bcf12c8e21879b78d4442782d8801e50d9f
vicky <jiajun_work@126.com>
2020-08-03 14:48:55 +0800

    [react-test][vicky] feature: add input to control amount.

--- src/components/CounterGroup/index.jsx
+++ src/components/CounterGroup/index.jsx
@@ -4,12 +4,24 @@ import Counter from '../Counter';
 class CounterGroud extends Component {
     constructor(props) {
         super(props);
-        this.size = 6;
+        this.state = {
+            size: 0
+        }
+    }
+
+    onResize = (event) => {
+        this.setState({
+            size: event.target.value ? parseInt(event.target.value) : 0
+        })
     }
 
     render() {
-        const initArray = [...Array(this.size).keys()];
+        const initArray = [...Array(this.state.size).keys()];
         return <div>
+            <label>
+                Group-size:
+                <input onBlur={this.onResize} defaultValue={0}></input>
+            </label>
             {
                 initArray.map(key => <Counter key={key} />)
             }
